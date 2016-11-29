5f394808b8f2c48f547a67647faa3a28c9c23ed6
VittorioPennella <v.pennella1@studenti.uniba.it>
2016-11-29 16:37:38 +0100

    Terminata una parte dell'esercitazione.

--- src/it/uniba/tdd/StringCalculator.java
+++ src/it/uniba/tdd/StringCalculator.java
@@ -1,9 +1,56 @@
 package it.uniba.tdd;
 
 public class StringCalculator {
-	public int add(String numbersStr) {
-		// Returns the sum of the numbers given in numbersStr
-		// not yet implemented
-		return 0;
+
+	public StringCalculator() {
 	}
+
+	public int add(String numbersStr) {
+		numbersStr = deleteNewLine(numbersStr);
+		int ris = 0;
+		int size = numbersStr.length();
+
+		if (size >= 1) {
+
+			String[] numbers;
+			numbers = numbersStr.split(",");
+			
+			
+			if (size == 1) {
+				ris = ignoreNumbersBiggerThan1000(numbers[0]);
+			} else {
+				for (int i = 0; i < numbers.length; i++) {
+					int j = ignoreNumbersBiggerThan1000(numbers[i]);
+					ris = ris + j;
+				}
+			}
+		}
+
+		else if (numbersStr.isEmpty()) {
+			ris = 0;
+		}
+
+		return ris;
+	}
+
+	public String deleteNewLine(String str) {
+		String string = str.replace("\n", ",");
+		return string;
+	}
+	
+
+	public String deleteAllDelimiter(String str) {
+		String string = "";
+		return string;
+	}
+	
+	public int ignoreNumbersBiggerThan1000(String str){
+		int ris = Integer.parseInt(str);
+		if(ris >= 1000){
+			ris = 0;
+		}
+		return ris;
+		
+	}
+
 }
--- src/it/uniba/tdd/tests/StringCalculatorTest.java
+++ src/it/uniba/tdd/tests/StringCalculatorTest.java
@@ -2,13 +2,131 @@ package it.uniba.tdd.tests;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import it.uniba.tdd.StringCalculator;
 
 public class StringCalculatorTest {
 
+	StringCalculator calc;
+	
+	@Before
+	public void setUp(){
+		calc = new StringCalculator();
+	}
+	
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testVoidStringReturnZero() {
+		//Act
+		int ris = calc.add("");
+		//Assert
+		assertEquals(0,ris);
+	}
+	
+	@Test
+	public void testStringReturnANumber() {
+		//Act
+		int ris = calc.add("3");
+		//Assert
+		assertEquals(3,ris);
+	}
+	
+	@Test
+	public void testStringReturnSumTwoNumbers() {
+		//Act
+		int ris = calc.add("3,2");
+		//Assert
+		assertEquals(5,ris);
+	}
+	
+	@Test
+	public void testStringReturnSumThreeNumbers() {
+		//Act
+		int ris = calc.add("3,2,1");
+		//Assert
+		assertEquals(6,ris);
+	}
+	
+	@Test
+	public void testStringReturnSumFourNumbers() {
+		//Act
+		int ris = calc.add("3,2,1,7");
+		//Assert
+		assertEquals(13,ris);
+	}
+	
+	@Test
+	public void testIgnoreNewLine() {
+		//Act
+		String ris = calc.deleteNewLine("1\n2,3");
+		//Assert
+		assertEquals("1,2,3",ris);
+	}
+	
+	@Test
+	public void testStringReturnSumOfNumbersWithOneNewLine() {
+		//Act
+		int ris = calc.add("1\n2,3");
+		//Assert
+		assertEquals(6,ris);
+	}
+	
+	@Test
+	public void testStringReturnSumOfNumbersWithTwoNewLine() {
+		//Act
+		int ris = calc.add("1\n2\n3");
+		//Assert
+		assertEquals(6,ris);
+	}
+	
+	@Test
+	public void testIgnoreNumberEqual1000() {
+		//Act
+		int ris = calc.ignoreNumbersBiggerThan1000("1000");
+		//Assert
+		assertEquals(0,ris);
+	}
+	
+	@Test
+	public void testIgnoreNumberEquals1200() {
+		//Act
+		int ris = calc.ignoreNumbersBiggerThan1000("1200");
+		//Assert
+		assertEquals(0,ris);
+	}
+	
+	@Test
+	public void testNotIgnoreNumbersUnder1000() {
+		//Act
+		int ris = calc.ignoreNumbersBiggerThan1000("12");
+		//Assert
+		assertEquals(12,ris);
+	}
+	
+	@Test
+	public void testSumWithNumbersBiggerThan1000() {
+		//Act
+		int ris = calc.add("12,1200,1");
+		//Assert
+		assertEquals(13,ris);
+	}
+	
+	@Test
+	public void testSumWithNumbersBiggerThan1000AndNewLine() {
+		//Act
+		int ris = calc.add("12,1200\n1");
+		//Assert
+		assertEquals(13,ris);
+	}
+	
+	@Test
+	public void testIgnoreNumberEquals1200a() {
+		//Act
+		int ris = calc.add("1200");
+		//Assert
+		assertEquals(0,ris);
 	}
 
 }
